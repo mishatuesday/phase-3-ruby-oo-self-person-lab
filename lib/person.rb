0b9e58704828eb0a9341df47671145a550e3d5b0
Misha Tuesday <mishatuesday@gmail.com>
2022-11-01 10:07:46 -0400

    commit tree 178b557d899e14f22be5b2cd0ef599e8a5ae2fdf

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_reader :happiness
+    attr_reader :name
+    attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        self.happiness = 8
+    end
+
+    def happiness=(num)
+        if num > 10
+            num = 10
+        elsif num < 0
+            num=0
+        end
+            @happiness = num
+    end
+
+end
+
+
+
